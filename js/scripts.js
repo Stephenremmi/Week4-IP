b22d9f3151602a5917014893e5df9e67f7298de0
ckorir <chrisruto.k@gmail.com>
2020-04-12 20:52:21 +0300

    added classes crust,size and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
-  $("button").click(function() {
-    $("#dev-hide").toggle();
-    $("#dev-show").toggle();
-  });
-});
+function name(firstName,secondName){
+  this.firstName = firstName;
+  this.secondName = secondName;
+}
+var name = new name()
+
+function small() {
+  $(".size3").show(500);
+
+  size = parseInt($("#small").val());
+
+  $(".sizesView").text("Small (Ksh " + size + ")");
+}
