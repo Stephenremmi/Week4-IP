eddd7752a26e27b63e53e18e899e26a6c1ff65e8
ckorir <chrisruto.k@gmail.com>
2020-04-14 17:20:26 +0300

    changed all buttons to radio input

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,29 @@ function pizza(pizzaSize,pizzaCrust,pizzaToppings){
     this.toppings = toppings
 }
 
+
+
+var small = document.getElementById("small").checked;
+var large = document.getElementById("large").checked;
+var medium = document.getElementById("medium").checked;
+var crispy = document.getElementById("crispy").checked;
+
+var smallValue = parseInt(small);
+var mediumValue = medium;
+var largeValue = parseInt(large);
+var crispyValue = parseInt(crispy);
+
+var smallValue = 450;
+var mediumValue = 750;
+var largeValue = 1000;
+var crispyValue = 100;
+
+function tt(){
+    if(small.checked === true && crispy.checked === true){
+        alert("hello");
+    }
+}
+
 function small(){
     document.getElementById("size1").innerHTML = "Small ------ ksh.450";
 }
