e859cc6683641459854834272c018a0e5e9c55e1
ckorir <chrisruto.k@gmail.com>
2020-04-14 15:32:52 +0300

    made size button clickable

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+function pizza(pizzaSize,pizzaCrust,pizzaToppings){
+    this.size = size
+    this.crust = crust
+    this.toppings = toppings
+}
+
+function small(){
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+function medium(){
+    document.getElementById("size2").innerHTML = "Medium ------ ksh.750";
+}
+function large(){
+    document.getElementById("size3").innerHTML = "Large ------ khs.1000";
+}
+function crispy(){
+    document.getElementById("crust1").innerHTML = "Hello World";
+}
+function stuffed(){
+    document.getElementById("crust2").innerHTML = "Hello World";
+}
+function glutten(){
+    document.getElementById("crust3").innerHTML = "Hello World";
+}
+function stuffed(){
+    document.getElementById("crust1").innerHTML = "Hello World";
+}
+function stuffed(){
+    document.getElementById("crust1").innerHTML = "Hello World";
+}
