b05cf651f1fa123fcce4cbd466b03b32ff37300d
ckorir <chrisruto.k@gmail.com>
2020-04-15 10:31:50 +0300

    added delivery choice

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,8 @@ function pizza(pizzaSize,pizzaCrust,pizzaToppings){
 }
 
 
-
-var small = document.getElementById("small").checked;
-var large = document.getElementById("large").checked;
-var medium = document.getElementById("medium").checked;
-var crispy = document.getElementById("crispy").checked;
-
-var smallValue = parseInt(small);
-var mediumValue = medium;
-var largeValue = parseInt(large);
-var crispyValue = parseInt(crispy);
-
-var smallValue = 450;
-var mediumValue = 750;
-var largeValue = 1000;
-var crispyValue = 100;
-
-function tt(){
-    if(small.checked === true && crispy.checked === true){
-        alert("hello");
-    }
-}
-
 function small(){
-    document.getElementById("size1").innerHTML = "Small ------ ksh.450";
+   document.getElementById("size1").innerHTML = "Small ------ ksh.450";
 }
 function medium(){
     document.getElementById("size2").innerHTML = "Medium ------ ksh.750";
@@ -37,7 +15,7 @@ function large(){
     document.getElementById("size3").innerHTML = "Large ------ khs.1000";
 }
 function crispy(){
-    document.getElementById("crust1").innerHTML = "Crispy ------ ksh.100";
+    document.getElementById("crust2").innerHTML = "Crispy ------ ksh.100";
 }
 function stuffed(){
     document.getElementById("crust2").innerHTML = "Stuffed ------ ksh.200";
@@ -54,3 +32,9 @@ function cheese(){
 function pepperoni(){
     document.getElementById("toppings3").innerHTML = "Pepperoni ------ ksh.100";
 }
+function deliver(){
+    prompt("Enter location");
+    prompt("Enter House Number");
+    document.getElementById("deliverfee").innerHTML = "Delivery ------ ksh.100";
+}
+
